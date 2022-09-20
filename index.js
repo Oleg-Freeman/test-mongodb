1fbd67ca3909b446b156944fbe29a4b3ebbe286a
Oleg Voitiuk <av.ssf4@gmail.com>
2022-09-20 19:42:08 +0300

    feat: express

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const { connect } = require("mongoose");
+const express = require("express");
 
 const { mongoURL } = require("./config");
+const app = express();
 
-connect(mongoURL, { dbName: "database" })
-  .then(() => console.log("MongoDb connected"))
-  .catch((err) => console.log(err.message));
+app.get("/", (req, res, next) => {
+  res.send("<h1>Main Page</h1>");
+});
+
+app.listen(5001, () => {
+  console.log("Server started");
+
+  connect(mongoURL, { dbName: "database" })
+    .then(() => console.log("MongoDb connected"))
+    .catch((err) => console.log(err.message));
+});
